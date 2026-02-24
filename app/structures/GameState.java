82f6763ec391461a70fb0eea1a31124f761f1d40
yaozhibin <632636875@qq.com>
2026-02-24 22:38:53 +0000

    Implement GameState structure for Unit Movement and Attack

--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -1,17 +1,37 @@
 package structures;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * This class can be used to hold information about the on-going game.
- * Its created with the GameActor.
- * 
- * @author Dr. Richard McCreadie
- *
+ * Holds state for an on-going game.
  */
 public class GameState {
 
-	
-	public boolean gameInitalised = false;
-	
-	public boolean something = false;
-	
+    public boolean gameInitalised = false;
+
+    // Selection state
+    public boolean hasSelectedUnit = false;
+    public int selectedUnitX = -1; // tile indices start at 1
+    public int selectedUnitY = -1;
+
+    // Highlight caches
+    public Set<String> validMoveTiles = new HashSet<>();
+    public Set<String> validAttackTiles = new HashSet<>();
+
+    // Prevent input during animations
+    public boolean isAnimating = false;
+
+    // Utility: key for sets
+    public static String key(int x, int y) {
+        return x + "," + y;
+    }
+
+    public void clearSelectionAndHighlights() {
+        hasSelectedUnit = false;
+        selectedUnitX = -1;
+        selectedUnitY = -1;
+        validMoveTiles.clear();
+        validAttackTiles.clear();
+    }
 }
